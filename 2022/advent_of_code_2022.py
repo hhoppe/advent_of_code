5ef926850fec3a030182811daa4459cf5496bece
Hugues Hoppe <hhoppe@gmail.com>
2023-01-07 00:04:55 -0800

    Tweak README and figures.

--- 2022/advent_of_code_2022.py
+++ 2022/advent_of_code_2022.py
@@ -16,24 +16,28 @@
 #
 # Here are some visualization results (obtained by setting `SHOW_BIG_MEDIA = True`):
 #
+# <p>
 # <a href="#day8">day8</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day08e.gif" width="240">&emsp;
 # <a href="#day9">day9</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day09b.gif" width="160">&emsp;
 # <a href="#day12">day12</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day12c.gif" width="300">
-# <br/><br/>
+# </p>
+# <p>
 # <a href="#day14">day14</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day14.gif" width="130">&emsp;
 # <a href="#day15">day15</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day15b.gif" width="180">&emsp;
 # <a href="#day16">day16</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day16b.gif" width="320">
-# <br/><br/>
+# </p>
+# <p>
 # <a href="#day17">day17</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day17.gif" width="200">&emsp;
 # <a href="#day18">day18</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day18a.gif" width="80">
-#  <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day18c.gif" width="100">&emsp;
+# <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day18c.gif" width="100">&emsp;
 # <a href="#day21">day21</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day21b.png" width="300">
-# <br/><br/>
-# <a href="#day22">day22</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day22.gif" width="150">&nbsp;
-# <a href="#day23">day23</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day23.gif" width="151">&nbsp;
+# </p>
+# <p>
+# <a href="#day22">day22</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day22.gif" width="160">&emsp;
+# <a href="#day23">day23</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day23.gif" width="151">&emsp;
 # <a href="#day24">day24</a> <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day24a.gif" width="30">&nbsp;
-#  <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day24c.gif" width="306">
-# <br/>
+# <img src="https://github.com/hhoppe/advent_of_code/raw/main/2022/results/day24c.gif" width="306">
+# </p>
 
 # %% [markdown]
 # <a name="preamble"></a>
